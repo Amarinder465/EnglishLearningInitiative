6965cfe4e9f2b7eb54d3d5ddbe46063694367bf7
Amarinder465 <amarindersingh465@gmail.com>
2024-02-10 19:26:04 -0800

    patch up

--- ELLI/src/environments/environment.prod.ts
+++ ELLI/src/environments/environment.prod.ts
@@ -1,12 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   firebase: {
-    projectId: 'elli-d071a',
-    appId: '1:180951791358:web:877a637f3a9376349bd7af',
-    storageBucket: 'elli-d071a.appspot.com',
-    apiKey: 'AIzaSyBIGaWTUU1fPubKlUn6OYUj_xPAEh0V_FU',
-    authDomain: 'elli-d071a.firebaseapp.com',
-    messagingSenderId: '180951791358',
-    measurementId: 'G-LDW2G2FL4Y',
+    apiKey: "AIzaSyA1RudnIZn65m99ZqN1V4H6ktGWeYKqGGQ",
+    authDomain: "elle-9d34b.firebaseapp.com",
+    projectId: "elle-9d34b",
+    storageBucket: "elle-9d34b.appspot.com",
+    messagingSenderId: "674340221754",
+    appId: "1:674340221754:web:0aab2d03ae592606409603",
+    measurementId: "G-6ZLLZ588T3"
   },
-  production: true
+  production: false,
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
--- ELLI/src/environments/environment.ts
+++ ELLI/src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebase: {
-    projectId: 'elli-d071a',
-    appId: '1:180951791358:web:877a637f3a9376349bd7af',
-    storageBucket: 'elli-d071a.appspot.com',
-    apiKey: 'AIzaSyBIGaWTUU1fPubKlUn6OYUj_xPAEh0V_FU',
-    authDomain: 'elli-d071a.firebaseapp.com',
-    messagingSenderId: '180951791358',
-    measurementId: 'G-LDW2G2FL4Y',
+    apiKey: "AIzaSyA1RudnIZn65m99ZqN1V4H6ktGWeYKqGGQ",
+    authDomain: "elle-9d34b.firebaseapp.com",
+    projectId: "elle-9d34b",
+    storageBucket: "elle-9d34b.appspot.com",
+    messagingSenderId: "674340221754",
+    appId: "1:674340221754:web:0aab2d03ae592606409603",
+    measurementId: "G-6ZLLZ588T3"
   },
   production: false,
 };
@@ -23,3 +23,5 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
